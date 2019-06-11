404d0e2c37fffdd1e1a48287c65513acbf6b4e48
Virasak Dungsrikaew <virasak@gmail.com>
2019-06-11 22:18:04 +0700

    cleanup

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
+    title: `Developer's Notes`,
+    author: `Virasak Dungrikaew`,
+    description: `A softeware developer who love functional programming, problem solving nad long-live learning`,
     siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `virasak`,
     },
   },
   plugins: [
